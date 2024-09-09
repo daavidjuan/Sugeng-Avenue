5863d4e510f3eaecade1378359d92db4602856ae
daavidjuan <ddavidjuann@gmail.com>
2024-09-09 22:54:28 +0700

    Modify views.py

--- main/models.py
+++ main/models.py
@@ -4,4 +4,4 @@ class MoodEntry(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-
+    rating = models.FloatField()
--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'name' : 'Adidas Samba',
+        'price': 'Rp. 1.850.000',
+        'description': 'Kalcer bro'
     }
 
     return render(request, "main.html", context)
